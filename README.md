0d9466aa83a0146c53df5c9a3935bb91f0a3fe49
Tho Nguyen <thosuperman@gmail.com>
2017-12-21 15:07:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Ethereum-Wallet-API
+=========
+
---
+++ test/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var assert = require('assert');
+var Wallet = require('../');
+var fixtures = require('./wallet');
+
+describe('Ethereum Wallet', function () {
+    var readOnlyWallet;
+
+    before(function () {
+        readOnlyWallet = Wallet.deserialize(JSON.stringify(fixtures));
+    });
+
+    it('should have more tests', function () {
+        assert.equal('hi', 'hi');
+    });
+
+    describe('serialization & deserialization', function () {
+        it('works', function () {
+            assert.deepEqual(fixtures, JSON.parse(readOnlyWallet.serialize()));
+        });
+    });
+
+    describe('createPrivateKey', function () {
+        it('works', function () {
+            var privateKey = readOnlyWallet.createPrivateKey(
+                '0xdd440046e5eb40de6cf06081c827dceb1aaf8794c030ac7e7b9bec505768750c'
+            );
+            assert(privateKey instanceof Buffer);
+            assert.equal(privateKey.length, 32);
+        });
+
+        it('errors on invalid private key', function () {
+            assert.throws(function () {
+                readOnlyWallet.createPrivateKey('123');
+            });
+        });
+    });
+
+    describe('exportPrivateKeys', function () {
+        it('works', function () {
+            var csv = readOnlyWallet.exportPrivateKeys();
+            assert.equal(typeof csv, 'string');
+            assert(csv.length > 0);
+        });
+    });
+
+});
